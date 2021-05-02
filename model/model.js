a8c7e9b5f78d547ef37067b2bd73d0d5ff946500
johnseydak <63112275+johnseydak@users.noreply.github.com>
2021-05-02 18:47:58 -0400

    update

--- model/model.js
+++ model/model.js
@@ -6,44 +6,114 @@ var homeContent = `<div class="home">
     <div class="position">Corporation CEO, books author.</div>
 </div>
 <div class="upcomingevents">
-    <div class="upcomingheadline">UPCOMING EVENTS</div>
+    <div class="upcomingheadline">UPCOMING EVENTS:</div>
+    <div class="headlineborder"></div>
     <div class="upcomingevents1">
         <div class="dateblock1">
-            <div class="date1"></div>
+            <div class="date1">
                 <div class="line1">06</div>
                 <div class="line2">JUN</div>
             </div>
+        </div>
         <div class="text1">
             <div class="blurb1"><p>Sed et persipiatis unde omnis iste natus</p></div>
             <div class="blurb2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos aspernatur</div>
         </div>
     </div>
-<div class="upcomingevents2">
-    <div class="dateblock2">30<br>JUL</div>
-    <div class="text1"><div class="blurb1"><p>Sed et persipiatis unde omnis iste natus</p></div>
-    <div class="blurb2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos aspernatur</div></div>
+    <div class="upcomingevents2">
+        <div class="dateblock2">
+            <div class="date2">
+                <div class="line3">30</div>
+                <div class="line4">JUL</div>
+            </div>
+        </div>
+        <div class="text2">
+            <div class="blurb3"><p>Sed et persipiatis unde omnis iste natus</p></div>
+            <div class="blurb4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos aspernatur</div>
+        </div>
+    </div>
+    <div class="upcomingevents3">
+        <div class="dateblock3">
+            <div class="date3">
+                <div class="line5">30</div>
+                <div class="line6">AUG</div>
+            </div>
+        </div>
+        <div class="text3">
+            <div class="blurb5"><p>Sed et persipiatis unde omnis iste natus</p></div>
+            <div class="blurb6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos aspernatur</div>
+        </div>
+    </div>
+    <div class="upcomingevents4">
+        <div class="dateblock4">
+            <div class="date4">
+                <div class="line7">23</div>
+                <div class="line8">NOV</div>
+            </div>
+        </div>    
+        <div class="text4">
+            <div class="blurb7"><p>Sed et persipiatis unde omnis iste natus</p></div>
+            <div class="blurb8">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos aspernatur</div>
+        </div>
+    </div>
+    <div class="upcomingevents5">
+        <div class="dateblock5">
+            <div class="date5">
+                <div class="line9">23</div>
+                <div class="line10">DEC</div>
+            </div>
+        </div>
+        <div class="text5">
+            <div class="blurb9"><p>Sed et persipiatis unde omnis iste natus</p></div>
+            <div class="blurb10">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos aspernatur</div>
+        </div>
+    </div>
+</div>`
+var aboutContent = `<div class="about">
+<div class="bannerimage"></div>
+<div class="ourhistory">OUR HISTORY:</div>
+<div class="border"></div>
+<div class="aboutpara">
+    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
 </div>
-<div class="upcomingevents3">
-    <div class="dateblock3">30<br>AUG</div>
-    <div class="text1"><div class="blurb1"><p>Sed et persipiatis unde omnis iste natus</p></div>
-    <div class="blurb2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos aspernatur</div></div>
 </div>
-<div class="upcomingevents4">
-    <div class="dateblock4">23<br>NOV</div>
-    <div class="text1"><div class="blurb1"><p>Sed et persipiatis unde omnis iste natus</p></div>
-    <div class="blurb2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos aspernatur</div></div>
+</div>`
+var galleryContent = `<div class="gallery">
+<div class="galleryheadline">GALLERY:</div>
+<div class="galleryborder"></div>
+<div class="gallerysection">
+    <div class="row1">
+        <div class="image1" a id="gallery1" href="#"></div>
+        <div class="image2" a id="gallery2" href="#"></div>
+        <div class="image3" a id="gallery3" href="#"></div> 
+    </div>
+    <div class="row2">
+        <div class="image4" a id="gallery4" href="#"></div>
+        <div class="image5" a id="gallery5" href="#"></div>
+    </div>
 </div>
-<div class="upcomingevents5">
-    <div class="dateblock5">23<br>DEC</div>
-    <div class="text1"><div class="blurb1"><p>Sed et persipiatis unde omnis iste natus</p></div>
-    <div class="blurb2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos aspernatur</div></div>
-</div>
-</div>` //paste homepage html inside quotations
-var aboutContent = ``//paste aboutpage html inside quotations
-var galleryContent = ``//paste page html inside quotations
+</div>`
 var blogContent = ``//paste homepage html inside quotations
 var contactContent = ``//paste homepage html inside quotations
- 
+var gallery1Content = `<div class="gallery1">
+<div class="galimage1">
+<div class="gallery1">
+<div class="galimage1">
+    <div class="gal1text1">1.</div>
+    <div class="gal1text2">food festival</div>
+
+</div>
+</div>
+</div>`
+var gallery2Content = ``
+var gallery3Content = ``
+var gallery4Content = ``
+var gallery5Content = ``
+var blog1Content = ``
+var blog2Content = ``
+var blog3Content = ``
+var blog4Content = ``
+var blog5Content = ``
 function getPageContent(pageId) {
 $("#app").html(eval(pageId));
 } //this _private function is getting the page Id but export makes it public
